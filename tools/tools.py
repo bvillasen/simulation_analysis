c355655e34de133a2f5f21d98d0ce2bd69aaaf52
Bruno <bvillasen@gmail.com>
2020-09-26 13:46:21 -0700

    added tools and load data

---
+++ tools/tools.py
@@ -0,0 +1,40 @@
+import os, sys
+from os import listdir
+from os.path import isfile, join
+import numpy as np
+import h5py as h5
+import time
+
+
+def print_mpi( text, rank, size,  mpi_comm):
+  for i in range(size):
+    if rank == i: print( text )
+    time.sleep( 0.01 )
+    mpi_comm.Barrier()
+
+def print_line_flush( terminalString ):
+  terminalString = '\r' + terminalString
+  sys.stdout. write(terminalString)
+  sys.stdout.flush() 
+
+
+def create_directory( dir ):
+  print(("Creating Directory: {0}".format(dir) ))
+  indx = dir[:-1].rfind('/' )
+  inDir = dir[:indx]
+  dirName = dir[indx:].replace('/','')
+  dir_list = next(os.walk(inDir))[1]
+  if dirName in dir_list: print( " Directory exists")
+  else:
+    os.mkdir( dir )
+    print( " Directory created")
+
+
+def get_files_names( fileKey, inDir, type='cholla' ):
+  if type=='nyx': dataFiles = [f for f in listdir(inDir) if (f.find(fileKey) >= 0 )  ]
+  if type == 'cholla': dataFiles = [f for f in listdir(inDir) if (isfile(join(inDir, f)) and (f.find(fileKey) >= 0 ) ) ]
+  dataFiles = np.sort( dataFiles )
+  nFiles = len( dataFiles )
+  # index_stride = int(dataFiles[1][len(fileKey):]) - int(dataFiles[0][len(fileKey):])
+  if type == 'nyx': return dataFiles, nFiles
+  if type == 'cholla': return dataFiles, nFiles
