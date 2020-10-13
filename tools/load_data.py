56f9826738b33a29db439bb1d297219619c55150
Bruno Villasenor <bvillasen@gmail.com>
2020-10-13 09:47:54 -0700

    new

--- tools/load_data.py
+++ tools/load_data.py
@@ -193,27 +193,27 @@ def load_snapshot_data_distributed( nSnap, inDir, data_type, fields, subgrid,  p
 
 #Load Snapshot Data
 
-# # dataDir = '/raid/bruno/data/'
-# dataDir = '/data/groups/comp-astro/bruno/'
-# inDir = dataDir + 'cosmo_sims/512_hydro_50Mpc/output_files_pchw18/'
-# 
-# 
-# n_snapshot = 169
-# 
-# data_type = 'hydro'
-# # data_type = 'particles'
-# 
-# fields = ['density']
-# 
-# precision = np.float32
-# 
-# Lbox = 5000    #kpc/h
-# proc_grid = [ 4, 2, 2]
-# box_size = [ Lbox, Lbox, Lbox ]
-# grid_size = [ 512, 512, 512 ] #Size of the simulation grid
-# subgrid = [ [0, 512], [0, 512], [0, 512] ] #Size of the volume to load
-# data = load_snapshot_data_distributed( n_snapshot, inDir, data_type, fields, subgrid,  precision, proc_grid,  box_size, grid_size, show_progess=True )
-# density = data[data_type]['density']  
+# dataDir = '/raid/bruno/data/'
+dataDir = '/data/groups/comp-astro/bruno/'
+inDir = dataDir + 'cosmo_sims/512_hydro_50Mpc/output_files_pchw18/'
+
+
+n_snapshot = 169
+
+data_type = 'hydro'
+# data_type = 'particles'
+
+fields = ['density']
+
+precision = np.float32
+
+Lbox = 5000    #kpc/h
+proc_grid = [ 4, 2, 2]
+box_size = [ Lbox, Lbox, Lbox ]
+grid_size = [ 512, 512, 512 ] #Size of the simulation grid
+subgrid = [ [0, 512], [0, 512], [0, 512] ] #Size of the volume to load
+data = load_snapshot_data_distributed( n_snapshot, inDir, data_type, fields, subgrid,  precision, proc_grid,  box_size, grid_size, show_progess=True )
+density = data[data_type]['density']  
 
 
   
