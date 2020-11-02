9e07a3999a561ad3d320a4baf82a633e8d959473
bruno villasenor <bvillasen@gmail.com>
2020-11-01 20:35:09 -0800

    added plot sim grid

---
+++ plot_simulation_grid.py
@@ -0,0 +1,14 @@
+import os, sys
+import numpy as np
+sys.path.append('tools')
+from tools import *
+#Append analysis directories to path
+extend_path()
+from parameters_UVB_rates import param_UVB_Rates
+from simulation_grid import Simulation_Grid
+
+root_dir   = '/data/groups/comp-astro/bruno/cosmo_sims/sim_grid/'
+output_dir = root_dir + 'figures/'
+create_directory( output_dir )
+
+SG = Simulation_Grid( parameters=param_UVB_Rates, sim_params=sim_params, job_params=job_params, dir=root_dir )
