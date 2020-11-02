0adbd22ccefddd809ff25738f38477a8b2748ea6
bruno villasenor <bvillasen@gmail.com>
2020-11-01 19:33:47 -0800

    new

--- simulation_grid/simulation_grid.py
+++ simulation_grid/simulation_grid.py
@@ -196,9 +196,15 @@ class Simulation_Grid:
     create_directory( fit_dir )
     
     files = [f for f in listdir(input_dir) if (isfile(join(input_dir, f)) and ( f.find('_analysis') > 0) ) ]
+    files.sort()
     n_files = len( files )
     print( f' N_Analysis_Files: {n_files}' )
     
+    for file in files:
+      n_snap = int(file.split('_')[0])
+      print( f' Fitting File: {n_file} ') 
+      data = load_analysis_data( n_file, input_dir )
+      values_to_fit = get_density_tyemperature_values_to_fit( data['phase_diagram'], delta_min=-1, delta_max=1, n_samples_line=50, fraction_enclosed=0.70 )
 
 
 
