76070fc104d52b42eb89724397887c12a0aac4ed
bruno villasenor <bvillasen@gmail.com>
2020-11-01 19:29:11 -0800

    new

--- simulation_grid/simulation_grid.py
+++ simulation_grid/simulation_grid.py
@@ -195,7 +195,7 @@ class Simulation_Grid:
     fit_dir = input_dir + 'fit_mcmc/'
     create_directory( fit_dir )
     
-    files = [f for f in listdir(inDir) if (isfile(join(inDir, f)) and ( f.find('_analysys') > 0) ) ]
+    files = [f for f in listdir(input_dir) if (isfile(join(input_dir, f)) and ( f.find('_analysys') > 0) ) ]
     n_files = len( files )
     print( f' N_Analysis_Files: {n_files}' )
     
