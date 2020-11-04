23424fe2c1214e38eebce2fae04175e41f742842
bruno villasenor <bvillasen@gmail.com>
2020-11-04 09:41:20 -0800

    added simulation grid

--- simulation_grid.py
+++ simulation_grid.py
@@ -12,10 +12,10 @@ create_directory( root_dir )
 
 
 SG = Simulation_Grid( parameters=param_UVB_Rates, sim_params=sim_params, job_params=job_params, dir=root_dir )
-# SG.Create_Grid_Directory_Structure()
-# SG.Create_Directories_for_Simulations()
-# SG.Create_All_Submit_Job_Scripts()
-# SG.Create_All_Parameter_Files()
-# SG.Create_UVB_Rates_Files()
+
+# SG.Submit_Simulation_Job( 0 )
 
 # SG.Get_Grid_Status()
+
+
+# SG.Fit_Grid_Phase_Diagram( )
