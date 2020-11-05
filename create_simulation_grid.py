790edd674f04d48ae043c33f3c9a8025f81f9b3f
bruno villasenor <bvillasen@gmail.com>
2020-11-04 17:16:07 -0800

    updated

--- create_simulation_grid.py
+++ create_simulation_grid.py
@@ -23,9 +23,11 @@ SG = Simulation_Grid( parameters=param_UVB_Rates, sim_params=sim_params, job_par
 
 # SG.Submit_Simulation_Job( 0 )
 
-sim_ids = SG.sim_ids
-for sim_id in sim_ids:
-  if sim_id == 0: continue
-  SG.Submit_Simulation_Job( sim_id )
-# 
-# SG.Get_Grid_Status()
+# sim_ids = SG.sim_ids
+# for sim_id in sim_ids:
+#   if sim_id == 0: continue
+#   SG.Submit_Simulation_Job( sim_id )
+ 
+
+
+SG.Get_Grid_Status()
