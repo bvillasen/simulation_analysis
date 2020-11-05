5141bb32249b57b931b69c4375cf9f2d0f2f2b32
bruno villasenor <bvillasen@gmail.com>
2020-11-04 17:13:17 -0800

    updated

--- create_simulation_grid.py
+++ create_simulation_grid.py
@@ -14,12 +14,14 @@ create_directory( root_dir )
 SG = Simulation_Grid( parameters=param_UVB_Rates, sim_params=sim_params, job_params=job_params, dir=root_dir )
 # SG.Create_Grid_Directory_Structure()
 # SG.Create_Directories_for_Simulations()
-SG.Create_All_Submit_Job_Scripts()
+# SG.Create_All_Submit_Job_Scripts()
 # SG.Create_All_Parameter_Files()
 # SG.Create_UVB_Rates_Files()
 
+# SG.Delete_Grid_Output_files()
 
-SG.Delete_Grid_Output_files()
+
+SG.Submit_Simulation_Job( 0 )
 
 # sim_ids = SG.Grid.sim_ids
 # for sim_id in sim_ids:
