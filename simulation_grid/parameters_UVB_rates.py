6ee50b18c4720a5bb666cd3d550141d7bc530bb8
bruno villasenor <bvillasen@gmail.com>
2020-11-08 15:35:27 -0800

    fixed

--- simulation_grid/parameters_UVB_rates.py
+++ simulation_grid/parameters_UVB_rates.py
@@ -11,7 +11,7 @@ param_UVB_Rates[0]['values'] = [ 1.0 ]
 param_UVB_Rates[1] = {}
 param_UVB_Rates[1]['key'] = 'B'
 param_UVB_Rates[1]['name'] = 'scale_H'
-param_UVB_Rates[1]['values'] = [ 1.3, 1.2, 1.1, 1. 0.9, 0.8, 0.7, 0.6 ]
+param_UVB_Rates[1]['values'] = [ 1.3, 1.2, 1.1, 1.0, 0.9, 0.8, 0.7, 0.6 ]
 
 param_UVB_Rates[2] = {}
 param_UVB_Rates[2]['key'] = 'C'
