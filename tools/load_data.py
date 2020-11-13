0884dd4abf548228681d7431026eee12085760bd
bruno <bvillasen@gmail.com>
2020-11-13 09:44:24 -0800

    fixed F_mean floor

--- tools/load_data.py
+++ tools/load_data.py
@@ -57,7 +57,7 @@ def load_analysis_data( n_file, input_dir, phase_diagram=True, lya_statistics=Tr
     lya_statistics = file['lya_statistics']
     data_out['lya_statistics']['n_skewers'] = lya_statistics.attrs['n_skewers'][0]
     F_mean = lya_statistics.attrs['Flux_mean'][0]
-    F_mean = min( F_mean, 1e-60 )
+    F_mean = max( F_mean, 1e-60 )
     data_out['lya_statistics']['Flux_mean'] = F_mean
     data_out['lya_statistics']['tau'] = -np.log( F_mean )
   
