cccb9ea5a68086cd250905214f447eb77f9b5ba3
bruno <bvillasen@gmail.com>
2020-11-19 19:25:08 -0800

    updated parameters

--- simulation_grid/parameters_UVB_rates.py
+++ simulation_grid/parameters_UVB_rates.py
@@ -6,7 +6,7 @@ param_UVB_Rates = {}
 param_UVB_Rates[0] = {}
 param_UVB_Rates[0]['key'] = 'A'
 param_UVB_Rates[0]['name'] = 'scale_He'
-param_UVB_Rates[0]['values'] = [ 0.3, 0.4, 0.5, 0.6  ]
+param_UVB_Rates[0]['values'] = [  0.36, 0.48, 0.60,  0.72  ]
 
 param_UVB_Rates[1] = {}
 param_UVB_Rates[1]['key'] = 'B'
@@ -16,7 +16,7 @@ param_UVB_Rates[1]['values'] = [ 0.7, 0.8, 0.9, 1.0 ]
 param_UVB_Rates[2] = {}
 param_UVB_Rates[2]['key'] = 'C'
 param_UVB_Rates[2]['name'] = 'deltaZ_He'
-param_UVB_Rates[2]['values'] = [ -0.1, 0.0, 0.1, 0.2   ]
+param_UVB_Rates[2]['values'] = [ -0.10, 0.02, 0.14, 0.26   ]
 
 param_UVB_Rates[3] = {}
 param_UVB_Rates[3]['key'] = 'D'
