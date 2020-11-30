218bacbda7ccd2a982f784423a9cc12a75a8c9e2
Bruno Villasenor <bvillasen@gmail.com>
2020-11-30 00:48:00 -0800

    updated

--- simulation_grid/parameters_UVB_rates.py
+++ simulation_grid/parameters_UVB_rates.py
@@ -6,12 +6,12 @@ param_UVB_Rates = {}
 param_UVB_Rates[0] = {}
 param_UVB_Rates[0]['key'] = 'A'
 param_UVB_Rates[0]['name'] = 'scale_H_photoion'
-param_UVB_Rates[0]['values'] = [  0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95   ]
+param_UVB_Rates[0]['values'] = [ 0.87 ]
 
 param_UVB_Rates[1] = {}
 param_UVB_Rates[1]['key'] = 'B'
 param_UVB_Rates[1]['name'] = 'scale_H_photoheat'
-param_UVB_Rates[1]['values'] = [ 0.87 ]
+param_UVB_Rates[1]['values'] = [ 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95 ]
 
 param_UVB_Rates[2] = {}
 param_UVB_Rates[2]['key'] = 'C'
