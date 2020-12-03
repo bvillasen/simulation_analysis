5bcd93dbbc0a55871529992529f1e090d8cb82e0
Bruno Villasenor <bvillasen@gmail.com>
2020-12-02 23:32:57 -0800

    fixed

--- simulation_grid/parameters_UVB_rates.py
+++ simulation_grid/parameters_UVB_rates.py
@@ -25,7 +25,7 @@ param_UVB_Rates[3] = {}
 param_UVB_Rates[3]['key'] = 'D'
 param_UVB_Rates[3]['name'] = 'deltaZ_H'
 # param_UVB_Rates[3]['values'] = [ -0.2, -0.1, 0.0,  ]
-param_UVB_Rates[3]['values'] = [ -0.4 -0.2,  0.0, 0.2, 0.4  ]
+param_UVB_Rates[3]['values'] = [ -0.4, -0.2,  0.0, 0.2, 0.4  ]
 
 
 
