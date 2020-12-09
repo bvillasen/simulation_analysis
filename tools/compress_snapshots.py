8f993c1f9cc056e6d7df9b710533c6da21c484ad
bruno <bvillasen@gmail.com>
2020-12-09 11:13:57 -0800

    updated

--- tools/compress_snapshots.py
+++ tools/compress_snapshots.py
@@ -8,8 +8,8 @@ import time
 # dataDir = '/home/bruno/Desktop/ssd_0/data/'
 # dataDir = '/gpfs/alpine/proj-shared/ast149/'
 dataDir = '/data/groups/comp-astro/bruno/'
-inDir = dataDir + 'cosmo_sims/256_hydro_50Mpc/output_files/'
-outDir = dataDir + 'cosmo_sims/256_hydro_50Mpc/snapshots_comparison_nyx/'
+input_dir = dataDir + 'cosmo_sims/256_hydro_50Mpc/output_files/'
+output_dir = dataDir + 'cosmo_sims/256_hydro_50Mpc/snapshots_comparison_nyx/'
 
 hydro = True
 particles = False
@@ -21,17 +21,17 @@ def split_name( file_name, part=False):
     nSnapshot = nSnapshot[:indx]
   return [int(nSnapshot), int(nBox)]
   
-print(( 'Input Dir: ' + inDir ))
-print(( 'Output Dir: ' + outDir ))
-create_directory( outDir )
+print(( 'Input Dir: ' + input_dir ))
+print(( 'Output Dir: ' + output_dir ))
+create_directory( output_dir )
 print("")
 
 name_base = 'h5'
 
 if hydro:
-  dataFiles = [f for f in listdir(inDir) if (isfile(join(inDir, f)) and (f.find('.h5.') > 0 ) and ( f.find('_particles') < 0) ) ]
+  dataFiles = [f for f in listdir(input_dir) if (isfile(join(input_dir, f)) and (f.find('.h5.') > 0 ) and ( f.find('_particles') < 0) ) ]
 else:
-  dataFiles = [f for f in listdir(inDir) if (isfile(join(inDir, f)) and ( f.find('_particles') > 0) ) ]
+  dataFiles = [f for f in listdir(input_dir) if (isfile(join(input_dir, f)) and ( f.find('_particles') > 0) ) ]
   
 dataFiles = np.sort( dataFiles )
 nFiles = len( dataFiles )
@@ -48,61 +48,57 @@ nBoxes = len( boxes )
 print(( "Number of snapshots: {0}".format(nSnapshots) ))
 print(( "Number of files per snapshot: {0}".format(nBoxes) ))
 
-# 
-# #Set wich snapshots to compress
-# snapshots_to_compress = snapshots_all
-# # n_per_index = 10
-# # snapshots_to_compress = snapshots_to_compress[index*n_per_index:(index+1)*n_per_index]
-# n_to_compress = len(snapshots_to_compress)
-# 
-# # snapshots_to_compress = [0]
-# 
-# print(( "\nNumber of snapshots to compres: {0}".format(n_to_compress) ))
+
+#Set wich snapshots to compress
+snapshots_to_compress = snapshots_all
+snapshots_to_compress = [0]
+n_to_compress = len(snapshots_to_compress)
+print(( "\nNumber of snapshots to compres: {0}".format(n_to_compress) ))
 # print(( ' {0}: {1}'.format( index, snapshots_to_compress ) ))
+
+
+#available Hydro Fields:
+#[ density, momentum_x, momentum_y, momentum_z, Enegy, GasEnergy ]
+#[ HI_density, HI_density, HeI_density, HeII_density, HeIII_density, e_density, metal_density, temperature, potential ]
+# hydro_fields = 'all'
+# hydro_fields = ['density' , 'momentum_x', 'HI_density', 'temperature']
+# hydro_fields = ['density' ,  'HI_density', 'temperature']
+# hydro_fields = ['density' ]
+hydro_fields = ['density' , 'GasEnergy' ]
+print(( "\nHydro fields: {0}".format(hydro_fields)))
+
+#available Particles Fields:
+#[ density, pos_x, pos_y, pos_z, vel_x, vel_y, vel_z, mass, particle_IDs ]
+# particles_fields = 'all'
+# particles_fields = ['density', 'vel_x', 'vel_y', 'vel_z' ]
+particles_fields = []
+print(( "\nParticles fields: {0}".format(particles_fields)))
+
+
+
+Lbox = 5000    #kpc/h
+n_points = 256
+proc_grid = [ 2, 2, 2]
+box_size = [ Lbox, Lbox, Lbox ]
+grid_size = [ n_points, n_points, n_points ] #Size of the simulation grid
+subgrid = [ [0, n_points], [0, n_points], [0, n_points] ] #Size of the volume to load
+# density = data[data_type]['density']  
 # 
-# # 
-# # n_proc_runs = (n_to_compress-1) // nprocs + 1
-# # proc_runs = np.array([ rank + i*nprocs for i in range(n_proc_runs) ])
-# # proc_runs = proc_runs[ proc_runs < n_to_compress ]
-# # if len(proc_runs) == 0: exit()
-# # print( ' {0}: {1}'.format( rank, proc_runs ) )
-# 
-# 
-# 
-# #available Hydro Fields:
-# #[ density, momentum_x, momentum_y, momentum_z, Enegy, GasEnergy ]
-# #[ HI_density, HI_density, HeI_density, HeII_density, HeIII_density, e_density, metal_density, temperature, potential ]
-# # hydro_fields = 'all'
-# # hydro_fields = ['density' , 'momentum_x', 'HI_density', 'temperature']
-# # hydro_fields = ['density' ,  'HI_density', 'temperature']
-# # hydro_fields = ['density' ]
-# hydro_fields = ['density' , 'momentum_x', 'momentum_y', 'momentum_z' ]
-# print(( "\nHydro fields: {0}".format(hydro_fields)))
-# 
-# #available Particles Fields:
-# #[ density, pos_x, pos_y, pos_z, vel_x, vel_y, vel_z, mass, particle_IDs ]
-# # particles_fields = 'all'
-# # particles_fields = ['density', 'vel_x', 'vel_y', 'vel_z' ]
-# particles_fields = ['density']
-# print(( "\nParticles fields: {0}".format(particles_fields)))
-# 
-# 
-# precision = np.float64
+
+precision = np.float64
 # # precision = np.float32
 # # precision = np.float16
-# print(( "\nPrecision: {0}".format( precision )))
-# 
-# print( "\nCompressing Snapshots..." )
+print(( "\nPrecision: {0}".format( precision )))
+
+print( "\nCompressing Snapshots..." )
 # for nSnap in snapshots_to_compress:
-#   start = time.time()
-#   if hydro:
-#     out_base_name = 'grid_' 
-#     compress_grid( nSnap, nBoxes, name_base, out_base_name, inDir, outDir, hydro_fields,  precision=precision )
-#   if particles:
-#     out_base_name = 'particles_' 
-#     compress_particles( nSnap, nBoxes, name_base, out_base_name, inDir, outDir, particles_fields, precision=precision )
-#   end = time.time()
-#   print(( ' Elapsed Time: {0:.2f} min'.format((end - start)/60.) ))
-# 
-#   # exit()
-# # 
+  start = time.time()
+  if hydro:
+    data = load_snapshot_data_distributed( n_snapshot, input_dir, 'hydro', hydro_fields, subgrid,  precision, proc_grid,  box_size, grid_size, show_progess=True )
+
+
+  if particles:
+    pass
+
+  end = time.time()
+  print(( ' Elapsed Time: {0:.2f} min'.format((end - start)/60.) ))
