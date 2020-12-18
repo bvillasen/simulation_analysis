155ce1e876c8808ee69b727e8cb788d441a30d04
Bruno Villasenor <bvillasen@gmail.com>
2020-12-18 11:14:32 -0600

    updated

--- lya_statistics/run_command_lux.py
+++ lya_statistics/run_command_lux.py
@@ -11,7 +11,7 @@ n_procs_per_node = 40
 lux_command = f'mpirun -n {n_procs} --map-by ppr:{n_procs_per_node}:node --oversubscribe {command} {params} '
 # lux_command = f'mpirunlux {n_procs} {n_procs_per_node} {command} {params}'
 print('Command: {0}'.format( lux_command ))
-process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+process = subprocess.Popen(lux_command, shell=True, stdout=subprocess.PIPE)
 for line in process.stdout:
   print(line)
 process.wait()
