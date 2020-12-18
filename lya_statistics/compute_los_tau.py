47411af37a3b3e42202b19ac7ced6237b3ef95a0
Bruno Villasenor <bvillasen@gmail.com>
2020-12-18 12:28:03 -0600

    uvb = hm12

--- lya_statistics/compute_los_tau.py
+++ lya_statistics/compute_los_tau.py
@@ -30,7 +30,8 @@ for option in parameters:
 if print_out: print( f'Snapshot: {n_snap}' )
 
 
-uvb = 'pchw18'
+# uvb = 'pchw18'
+uvb = 'hm12'
 # dataDir = '/raid/bruno/data/'
 # dataDir = '/home/bruno/Desktop/ssd_0/data/'
 dataDir = '/data/groups/comp-astro/bruno/'
