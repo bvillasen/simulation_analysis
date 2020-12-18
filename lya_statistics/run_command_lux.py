a7f333c2ad0a53c22e4e7024d2ca8bc5ceed6891
Bruno Villasenor <bvillasen@gmail.com>
2020-12-18 11:12:51 -0600

    fixed

--- lya_statistics/run_command_lux.py
+++ lya_statistics/run_command_lux.py
@@ -8,8 +8,8 @@ params = f'compute_los_tau.py n_snap={n_snap}'
 n_procs = 40
 n_procs_per_node = 40
 
-# lux_command = f'mpirun -n {n_procs} --map-by ppr:{n_procs_per_node}:node --oversubscribe {command} '
-lux_command = f'mpirunlux {n_procs} {n_procs_per_node} {command} {params}'
+lux_command = f'mpirun -n {n_procs} --map-by ppr:{n_procs_per_node}:node --oversubscribe {command} '
+# lux_command = f'mpirunlux {n_procs} {n_procs_per_node} {command} {params}'
 print('Command: {0}'.format( lux_command ))
 process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
 for line in process.stdout:
