cadd42fa58629883020dc7bdb6bc8e78a0d84ea5
Bruno Villasenor <bvillasen@gmail.com>
2020-12-18 12:04:14 -0600

    updated

--- lya_statistics/run_command_lux.py
+++ lya_statistics/run_command_lux.py
@@ -7,19 +7,18 @@ snapshots = list( set( snaps_boss ).union(set(snaps)))
 snapshots.sort()
 print(snapshots)
 
-# 
-# n_snap = 169
-# command = 'python'
-# params = f'compute_los_tau.py n_snap={n_snap}'
-# 
-# n_procs = 40
-# n_procs_per_node = 40
-# 
-# lux_command = f'mpirun -n {n_procs} --map-by ppr:{n_procs_per_node}:node --oversubscribe {command} {params} '
-# # lux_command = f'mpirunlux {n_procs} {n_procs_per_node} {command} {params}'
-# print('Command: {0}'.format( lux_command ))
-# process = subprocess.Popen(lux_command, shell=True, stdout=subprocess.PIPE)
-# for line in process.stdout:
-#   print(line)
-# process.wait()
-# print(process.returncode)
+for n_snap in snapshots:
+  command = 'python'
+  params = f'compute_los_tau.py n_snap={n_snap}'
+
+  n_procs = 40
+  n_procs_per_node = 40
+
+  lux_command = f'mpirun -n {n_procs} --map-by ppr:{n_procs_per_node}:node --oversubscribe {command} {params} '
+  # lux_command = f'mpirunlux {n_procs} {n_procs_per_node} {command} {params}'
+  print('Command: {0}'.format( lux_command ))
+  process = subprocess.Popen(lux_command, shell=True, stdout=subprocess.PIPE)
+  for line in process.stdout:
+    print(line)
+  process.wait()
+  print(process.returncode)
