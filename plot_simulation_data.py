e5338146b653ec6b050da39ea10f395e428dad7f
bruno villasenor <bvillasen@gmail.com>
2021-01-14 16:19:25 -0800

    updated plot sim_data

--- plot_simulation_data.py
+++ plot_simulation_data.py
@@ -14,15 +14,16 @@ from plot_power_spectrum_functions import plot_power_spectrum_grid
 output_dir = root_dir + 'figures/'
 create_directory( output_dir )
 
-SG = Simulation_Grid( parameters=param_UVB_Rates, sim_params=sim_params, job_params=job_params, dir=root_dir )
-sim_ids = SG.sim_ids
-SG.Load_Grid_Analysis_Data( sim_ids=sim_ids, load_fit=True )
+# SG = Simulation_Grid( parameters=param_UVB_Rates, sim_params=sim_params, job_params=job_params, dir=root_dir )
+# sim_ids = SG.sim_ids
+# SG.Load_Grid_Analysis_Data( sim_ids=sim_ids, load_fit=True )
+# 
+# sim_id = 0
+# sim_data  = SG.Grid[sim_id]['analysis']
+# sim_param = SG.Grid[sim_id]['parameters'] 
 
-sim_id = 0
-sim_data  = SG.Grid[sim_id]['analysis']
-sim_param = SG.Grid[sim_id]['parameters'] 
 
-
+sim_data = None
 ps_data_dir = 'lya_statistics/data/'
 plot_power_spectrum_grid( ps_data_dir, output_dir, scales='small', sim_data=sim_data )
 plot_power_spectrum_grid( ps_data_dir, output_dir, scales='large', sim_data=sim_data )
