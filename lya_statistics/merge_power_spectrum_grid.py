68f1e0a8f3295081546072aabac6728e42fc9338
bruno <bvillasen@gmail.com>
2021-01-20 10:22:49 -0800

    new

--- lya_statistics/merge_power_spectrum_grid.py
+++ lya_statistics/merge_power_spectrum_grid.py
@@ -59,9 +59,11 @@ for index, file_name in enumerate( file_list ):
   file.close()
   
   if not allocated_memory:
+    print( f'Subgrid size: {subgrid_shape} ' )
     n_i, n_j, n_bins = subgrid_shape
     full_size = ( n_i, n_j* n_files, n_bins )
-    PS_grid  = np.ones( full_size ) * -1
+    print( f' Full Size: {full_size}' )
+    # PS_grid  = np.ones( full_size ) * -1
     allocated_memory = True 
 
     
