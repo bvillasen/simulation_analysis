eb4db150f725594c629c7f2996890c2e6c3c501b
bruno <bvillasen@gmail.com>
2021-01-27 13:03:36 -0800

    updated to load snapshot

--- tools/load_data.py
+++ tools/load_data.py
@@ -275,7 +275,7 @@ def load_snapshot_data_distributed( data_type, fields,  nSnap, inDir,  box_size,
     else:
       data_all = np.concatenate( data_all )
       data_out[field] = data_all
-      if field == 'particle_IDs': data_out[field] = data_out[field].astype( np.int ) 
+      # if field == 'particle_IDs': data_out[field] = data_out[field].astype( np.int ) 
     if show_progess: print("")
   return data_out
 
@@ -317,34 +317,34 @@ def load_cholla_snapshot_file( nSnap, inDir, cool=False, dm=True, cosmo=True, hy
 
   return outDir
 
-# 
-# #Example for Loading  Snapshot Data (Below)
-# 
-# data_dir = '/data/groups/comp-astro/bruno/'
-# input_dir = data_dir + 'cosmo_sims/256_hydro_50Mpc_halo_tests/output_files/'
-# 
-# 
-# precision = np.float64
-# Lbox = 50000.0    #kpc/h
-# n_cells = 256
-# box_size = [ Lbox, Lbox, Lbox ]
-# grid_size = [ n_cells, n_cells, n_cells ] #Size of the simulation grid
-# 
-# n_snapshot = 0
-# 
-# #Load Gas data
-# fields = [ 'density' ]
-# data_gas = load_snapshot_data_distributed( 'hydro', fields, n_snapshot, input_dir, box_size, grid_size,  precision, show_progess=True )
-# current_z = data_gas['Current_z']  #redshift
-# density_gas = data_gas['density']  # h^2 Msun / kpc^3
-# 
-# #Load DM data
-# fields = [ 'density', 'pos_x', 'pos_y', 'pos_z', 'particle_IDs' ]
-# data_dm = load_snapshot_data_distributed( 'particles', fields, n_snapshot, input_dir, box_size, grid_size,  precision, show_progess=True, print_fields=True )
-# particle_mass = data_dm['particle_mass'] #h^-1 Msun 
-# density_dm = data_dm['density']          # h^2 Msun / kpc^3
-# pos_x = data_dm['pos_x']                 #h^-1 kpc
-# pos_y = data_dm['pos_y']                 #h^-1 kpc
-# pos_z = data_dm['pos_z']                 #h^-1 kpc
-# p_ids = data_dm['particle_IDs']      
-# 
+
+#Example for Loading  Snapshot Data (Below)
+
+data_dir = '/data/groups/comp-astro/bruno/'
+input_dir = data_dir + 'cosmo_sims/256_hydro_50Mpc_halo_tests/output_files/'
+
+
+precision = np.float64
+Lbox = 50000.0    #kpc/h
+n_cells = 256
+box_size = [ Lbox, Lbox, Lbox ]
+grid_size = [ n_cells, n_cells, n_cells ] #Size of the simulation grid
+
+n_snapshot = 0
+
+#Load Gas data
+fields = [ 'density' ]
+data_gas = load_snapshot_data_distributed( 'hydro', fields, n_snapshot, input_dir, box_size, grid_size,  precision, show_progess=True )
+current_z = data_gas['Current_z']  #redshift
+density_gas = data_gas['density']  # h^2 Msun / kpc^3
+
+#Load DM data
+fields = [ 'density', 'pos_x', 'pos_y', 'pos_z', 'particle_IDs' ]
+data_dm = load_snapshot_data_distributed( 'particles', fields, n_snapshot, input_dir, box_size, grid_size,  precision, show_progess=True, print_fields=True )
+particle_mass = data_dm['particle_mass'] #h^-1 Msun 
+density_dm = data_dm['density']          # h^2 Msun / kpc^3
+pos_x = data_dm['pos_x']                 #h^-1 kpc
+pos_y = data_dm['pos_y']                 #h^-1 kpc
+pos_z = data_dm['pos_z']                 #h^-1 kpc
+p_ids = data_dm['particle_IDs']      
+
