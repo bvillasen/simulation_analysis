b4388789a2e9b5ce63b91618b1ef791c053c79d4
bruno <bvillasen@gmail.com>
2021-02-04 15:58:13 -0800

    updated

--- lya_statistics/compute_los_tau.py
+++ lya_statistics/compute_los_tau.py
@@ -62,7 +62,7 @@ cosmology['Omega_L'] = 0.6889
 
 chem_type = 'HeII'
 
-n_skewers_total = 6000
+n_skewers_total = 12000
 n_skewers_axis = n_skewers_total// 3 
 n_skewers_list = [ n_skewers_axis, n_skewers_axis, n_skewers_axis ]
 axis_list = [ 'x', 'y', 'z' ]
