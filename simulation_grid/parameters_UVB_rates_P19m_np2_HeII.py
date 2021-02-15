95547891c0eff5aa129368dfc52a4a91b858f6b6
bruno <bvillasen@gmail.com>
2021-02-14 17:18:29 -0800

    updated

--- simulation_grid/parameters_UVB_rates_P19m_np2_HeII.py
+++ simulation_grid/parameters_UVB_rates_P19m_np2_HeII.py
@@ -7,11 +7,12 @@ param_UVB_Rates = {}
 param_UVB_Rates[0] = {}
 param_UVB_Rates[0]['key'] = 'A'
 param_UVB_Rates[0]['name'] = 'scale_He_photoion'
-param_UVB_Rates[0]['values'] = [   0.3, 0.4, 0.5, 0.6, 0.7, 0.8  ]
+# param_UVB_Rates[0]['values'] = [   0.3, 0.4, 0.5, 0.6, 0.7, 0.8  ]
+param_UVB_Rates[0]['values'] = [ 1.0 ]
 
 param_UVB_Rates[1] = {}
 param_UVB_Rates[1]['key'] = 'B'
 param_UVB_Rates[1]['name'] = 'scale_He_photoheat'
-param_UVB_Rates[1]['values'] = [  1.0 ]
+param_UVB_Rates[1]['values'] = [   0.3, 0.4, 0.5, 0.6, 0.7, 0.8  ] ]
 
 
