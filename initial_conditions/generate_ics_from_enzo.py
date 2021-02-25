654463fd72d952178c074629502ac63de11109ad
bruno <bvillasen@gmail.com>
2021-02-24 20:25:03 -0800

    particles ics

--- initial_conditions/generate_ics_from_enzo.py
+++ initial_conditions/generate_ics_from_enzo.py
@@ -27,8 +27,8 @@ print(f'Input Dir: {input_dir}' )
 print(f'Output Dir: {output_dir}' )
 create_directory( output_dir )
 
-hydro = True
-particles = False
+hydro = False
+particles = True
 
 
 nSnap = 0
