1c643794d6890f7c1ed0e1aefde9995d13340c2b
bruno villasenor <bvillasen@gmail.com>
2021-03-01 09:00:46 -0800

    updated for 22 node runs on summit

--- simulation_grid/simulation_parameters.py
+++ simulation_grid/simulation_parameters.py
@@ -74,7 +74,7 @@ if system == 'Lux':
   if n_points == 512:  sim_params['indir'] = ics_dir + f'512_50Mpc/ics_8_z20/'
   if n_points == 1024: sim_params['indir'] = ics_dir + f'1024_50Mpc/ics_16_z20/'
 if system == 'Summit':
-  if n_points == 1024: sim_params['indir'] = ics_dir + f'1024_50Mpc/ics_64_z16/'
+  if n_points == 1024: sim_params['indir'] = ics_dir + f'1024_50Mpc/ics_128_z16/'
 if system == 'Lux':    sim_params['scale_outputs_file'] = cholla_dir + 'scale_output_files/outputs_single_output_z2.txt'
 if system == 'Summit': sim_params['scale_outputs_file'] = cholla_dir + 'scale_output_files/outputs_cosmo_15.txt'
 sim_params['analysis_scale_outputs_file'] = cholla_dir + 'scale_output_files/outputs_cosmo_analysis_56.txt'
@@ -98,8 +98,8 @@ if system == 'Lux':
 
 if system == 'Summit':
   if n_points == 1024:
-    job_params['n_mpi'] = 64
-    job_params['n_nodes'] = 11
+    job_params['n_mpi'] = 128
+    job_params['n_nodes'] = 22
     
 if system == 'Lux':    
   job_params['n_tasks_per_node'] = 2
