019a69fadf36e0abd13e234aef8e50be5432f818
bruno villasenor <bvillasen@gmail.com>
2021-03-01 20:41:38 -0800

    restored lux grid

--- simulation_grid/simulation_parameters.py
+++ simulation_grid/simulation_parameters.py
@@ -12,10 +12,10 @@ n_points = 1024
 # grid_name = f'{n_points}_P19m_np3_nsim64'
 # grid_name = f'{n_points}_P19m_np2_HeII'
 # grid_name = f'{n_points}_P19'
-grid_name = f'{n_points}_P19m'
+# grid_name = f'{n_points}_P19m'
 # grid_name = f'{n_points}_P19m_np3_nsim8'
 # grid_name = f'{n_points}_P19m_np3_nsim8_v2'
-# grid_name = f'{n_points}_P19m_np4_nsim16'
+grid_name = f'{n_points}_P19m_np4_nsim16'
 
 
 if system == 'Lux':
