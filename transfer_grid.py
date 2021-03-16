6f9c3737b5a633360a646e1f311e840540624730
bruno <bvillasen@gmail.com>
2021-03-16 15:02:47 -0700

    new

--- transfer_grid.py
+++ transfer_grid.py
@@ -1,10 +1,12 @@
 import sys, os
 import numpy
+from shutil import copyfile
 sys.path.append('tools')
 from tools import *
 #Append analysis directories to path
 extend_path()
 from transfer_grid_functions import *
+
 
 data_dir = '/data/groups/comp-astro/bruno/cosmo_sims/sim_grid/'
 src_dir = data_dir + '1024_P19m_np4_nsim256/' 
@@ -49,6 +51,7 @@ for sim_id in range( n_dst_sims ):
     src_dir_short = src_dir[src_dir.find('sim_grid')+9:]+'/'
     dst_dir_short = dst_dir[dst_dir.find('sim_grid')+9:]+'/' 
     for file in files_to_copy:
+      copyfile(src_dir + '/' + file, dst_dir + '/' + file )
       print( f' Copied  {src_dir_short+file} -> {dst_dir_short+file} ' )
     
     if copy_reduced_files:
@@ -68,6 +71,6 @@ for sim_id in range( n_dst_sims ):
 
 
 
-print( f'Successfully Transfered Grid:   n_copied:{n_copied}    n_skipped:{n_skipped}')
+print( f'\nSuccessfully Transfered Grid:   n_copied:{n_copied}    n_skipped:{n_skipped}')
 
 
