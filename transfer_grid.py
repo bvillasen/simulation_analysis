fcd3c99f8905d9d9e5cd8413001e2a646ea587d2
bruno <bvillasen@gmail.com>
2021-03-16 12:42:08 -0700

    updated

---
+++ transfer_grid.py
@@ -0,0 +1,13 @@
+import os, sys
+import numpy
+import h5py as h5
+
+
+data_dir = '/data/groups/comp-astro/bruno/cosmo_sims/sim_grid/'
+src_dir = data_dir + '1024_P19m_np4_nsim256/' 
+dst_dir = data_dir + '1024_P19m_np4_nsim320/'
+
+
+
+grid_dir = src_dir
+sim_dirs = [f for f in listdir(grid_dir) if (isdir(join(grid_dir, f)) and (f[0] == 'S' ) ) ]
