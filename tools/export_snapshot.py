2fbe1e2800e2cf690bcea9aa1fe3c9e2acb5e62f
bruno <bvillasen@gmail.com>
2021-03-19 09:44:38 -0700

    export

---
+++ tools/export_snapshot.py
@@ -0,0 +1,52 @@
+import os, sys
+from os import listdir
+from os.path import isfile, join
+import h5py as h5
+import numpy as np
+from load_data import load_snapshot_data_distributed
+from tools import *
+#Example for Loading  Snapshot Data (Below)
+
+data_dir = '/data/groups/comp-astro/bruno/'
+input_dir = data_dir + 'cosmo_sims/2048_hydro_50Mpc/output_files_pchw18/'
+output_dir = data_dir + 'cosmo_sims/2048_hydro_50Mpc/snapshot_files/'
+create_directory( output_dir )
+
+precision = np.float32
+Lbox = 50000.0    #kpc/h
+n_cells = 2048
+box_size = [ Lbox, Lbox, Lbox ]
+grid_size = [ n_cells, n_cells, n_cells ] #Size of the simulation grid
+proc_grid = [ 8, 8, 8 ]
+
+n_snapshot = 169
+
+
+out_file_name = f'snapshot_{n_snapshot:03}.h5'
+out_file = h5.File( out_file_name, 'w' )
+
+
+#Load Gas data
+fields = [ 'density' ]
+data_gas = load_snapshot_data_distributed( 'particles', fields, n_snapshot, input_dir, box_size, grid_size,  precision, show_progess=True, proc_grid=proc_grid, print_fields=True )
+current_z = data_gas['Current_z']  #redshift
+density_gas = data_gas['density']  # h^2 Msun / kpc^3
+
+
+
+
+out_file.attrs['current_z'] = current_z
+
+out_file.close()
+
+#Load DM data
+# fields = [ 'density', 'pos_x', 'pos_y', 'pos_z', 'particle_IDs' ]
+# fields = [ 'density',  ]
+# data_dm = load_snapshot_data_distributed( 'particles', fields, n_snapshot, input_dir, box_size, grid_size,  precision, show_progess=True, print_fields=True )
+# particle_mass = data_dm['particle_mass'] #h^-1 Msun 
+# density_dm = data_dm['density']          # h^2 Msun / kpc^3
+# pos_x = data_dm['pos_x']                 #h^-1 kpc
+# pos_y = data_dm['pos_y']                 #h^-1 kpc
+# pos_z = data_dm['pos_z']                 #h^-1 kpc
+# p_ids = data_dm['particle_IDs']      
+
--- tools/load_snapshot.py
+++ tools/load_snapshot.py
@@ -8,33 +8,32 @@ from load_data import load_snapshot_data_distributed
 #Example for Loading  Snapshot Data (Below)
 
 data_dir = '/data/groups/comp-astro/bruno/'
-input_dir = data_dir + 'cosmo_sims/2048_hydro_50Mpc/output_files_pchw18/'
-# input_dir = data_dir + 'cosmo_sims/halo_tests/256_hydro_50Mpc/output_files/'
+input_dir = data_dir + 'cosmo_sims/halo_tests/256_hydro_50Mpc/output_files/'
+# input_dir = data_dir + 'cosmo_sims/2048_hydro_50Mpc/output_files_pchw18/'
 
 
-precision = np.float32
+precision = np.float64
 Lbox = 50000.0    #kpc/h
-n_cells = 2048
+n_cells = 256
 box_size = [ Lbox, Lbox, Lbox ]
 grid_size = [ n_cells, n_cells, n_cells ] #Size of the simulation grid
-proc_grid = [ 8, 8, 8 ]
 
 n_snapshot = 169
 
 #Load Gas data
 fields = [ 'density' ]
-data_gas = load_snapshot_data_distributed( 'hydro', fields, n_snapshot, input_dir, box_size, grid_size,  precision, show_progess=True, proc_grid=proc_grid )
+data_gas = load_snapshot_data_distributed( 'hydro', fields, n_snapshot, input_dir, box_size, grid_size,  precision, show_progess=True )
 current_z = data_gas['Current_z']  #redshift
 density_gas = data_gas['density']  # h^2 Msun / kpc^3
 
-#Load DM data
-# fields = [ 'density', 'pos_x', 'pos_y', 'pos_z', 'particle_IDs' ]
-# fields = [ 'density',  ]
-# data_dm = load_snapshot_data_distributed( 'particles', fields, n_snapshot, input_dir, box_size, grid_size,  precision, show_progess=True, print_fields=True )
-# particle_mass = data_dm['particle_mass'] #h^-1 Msun 
-# density_dm = data_dm['density']          # h^2 Msun / kpc^3
-# pos_x = data_dm['pos_x']                 #h^-1 kpc
-# pos_y = data_dm['pos_y']                 #h^-1 kpc
-# pos_z = data_dm['pos_z']                 #h^-1 kpc
-# p_ids = data_dm['particle_IDs']      
+Load DM data
+fields = [ 'density', 'pos_x', 'pos_y', 'pos_z', 'particle_IDs' ]
+fields = [ 'density',  ]
+data_dm = load_snapshot_data_distributed( 'particles', fields, n_snapshot, input_dir, box_size, grid_size,  precision, show_progess=True, print_fields=True )
+particle_mass = data_dm['particle_mass'] #h^-1 Msun 
+density_dm = data_dm['density']          # h^2 Msun / kpc^3
+pos_x = data_dm['pos_x']                 #h^-1 kpc
+pos_y = data_dm['pos_y']                 #h^-1 kpc
+pos_z = data_dm['pos_z']                 #h^-1 kpc
+p_ids = data_dm['particle_IDs']      
 
