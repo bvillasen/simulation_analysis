e42ce30cb2cb8ee7ae90571fefe3299f927aac01
bruno <bvillasen@gmail.com>
2021-04-05 21:16:21 -0700

    updated for sim300

--- simulation_grid/simulation_parameters.py
+++ simulation_grid/simulation_parameters.py
@@ -17,8 +17,8 @@ n_points = 1024
 # grid_name = f'{n_points}_P19m_np3_nsim8_v2'
 # grid_name = f'{n_points}_P19m_np4_nsim16'
 # grid_name = f'{n_points}_P19m_np4_nsim256'
-# grid_name = f'{n_points}_P19m_np4_nsim320'
-grid_name = f'{n_points}_P19m_np4_nsim400'
+grid_name = f'{n_points}_P19m_np4_nsim320'
+# grid_name = f'{n_points}_P19m_np4_nsim400'
 
 # load_reduced_files = True
 load_reduced_files = False
