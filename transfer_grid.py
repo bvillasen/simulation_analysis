4a1fcb24e784603c31c28a32bd9157bcd07eb16a
bruno <bvillasen@gmail.com>
2021-04-06 01:38:58 -0700

    fixed

--- transfer_grid.py
+++ transfer_grid.py
@@ -100,11 +100,13 @@ for sim_id in dst_ids_to_transfer:
           if len( os.listdir(dst_mcmc) ) == 0:
             print( f' Deleting Empty: {dst_red_dir + "/analysis_files/fit_mcmc"}')
             os.rmdir( dst_red_dir + '/analysis_files/fit_mcmc' )
-            print( f' Deleting Empty: {dst_red_dir + "/analysis_files"}')
-            os.rmdir( dst_red_dir + '/analysis_files' )
             copy_directory = True
-          else:
-            copy_directory = False
+        else:
+          print( f' Deleting Empty: {dst_red_dir + "/analysis_files"}')
+          os.rmdir( dst_red_dir + '/analysis_files' )
+          copy_directory = True
+      else:
+        copy_directory = False
       if copy_directory:
         print( f' Deleting Empty: { dst_red_dir }')
         os.rmdir( dst_red_dir )
