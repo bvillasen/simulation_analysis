ab86468d262bd54857e09a6c77c2dc7cdbd99c1b
bruno villasenor <bvillasen@gmail.com>
2021-05-17 12:31:23 -0700

    fixed

--- initial_conditions/run_ics_command.py
+++ initial_conditions/run_ics_command.py
@@ -34,7 +34,7 @@ else:
 n_fields = len( fields_list )
 indices_local = split_indices( range(n_fields), rank, n_procs )
 fields_local = [ fields_list[i] for i in indices_local ]
-printf( f'proc_id: {rank}  fields:{fields_local}')   
+print( f'proc_id: {rank}  fields:{fields_local}')   
 
   
   
