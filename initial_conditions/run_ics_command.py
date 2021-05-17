45831d075c2b513754a45723094b45fe168082b2
bruno villasenor <bvillasen@gmail.com>
2021-05-17 12:26:28 -0700

    mpi ics

--- initial_conditions/run_ics_command.py
+++ initial_conditions/run_ics_command.py
@@ -1,7 +1,9 @@
 import subprocess
+tools_dir = root_dir + 'tools'
+sys.path.append( tools_dir )
+from tools import split_indices
 
-
-fields_particles_list = [ 'pos_y', 'pos_z', 'vel_x', 'vel_y', 'vel_z' ]
+fields_particles_list = [ 'mass', 'pos_x', 'pos_y', 'pos_z', 'vel_x', 'vel_y', 'vel_z' ]
 
 fields_hydro_list = [ 'density', 'velocity_x', 'velocity_y', 'velocity_z', 'thermal_energy'  ]
 
@@ -16,12 +18,33 @@ if type == 'particles': fields_list = fields_particles_list
 if type == 'hydro': fields_list = fields_hydro_list
 if merge: fields_list = ['merge']
 
-for field in fields_list:
-  command = f'python generate_ics_from_enzo_distributed.py type={type} field={field} L_MPC={L_MPC}'
-  print(f'Command: {command}')
-  process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-  for line in process.stdout:
-    print(line)
-  process.wait()
-  print(process.returncode)
- 
+use_mpi = True
+if use_mpi:
+  from mpi4py import MPI
+  comm = MPI.COMM_WORLD
+  rank = comm.Get_rank()
+  n_procs = comm.Get_size()
+else:
+  rank = 0
+  n_procs = 1
+
+n_fields = len( fields_list )
+indices_local = split_indices( range(n_fields), rank, n_procs )
+fields_local = fields_list[indices_local] 
+
+printf( f'proc_id: {rank}  fields:{fields_local})   
+  
+  
+  
+
+
+
+# for field in fields_list:
+#   command = f'python generate_ics_from_enzo_distributed.py type={type} field={field} L_MPC={L_MPC}'
+#   print(f'Command: {command}')
+#   process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+#   for line in process.stdout:
+#     print(line)
+#   process.wait()
+#   print(process.returncode)
+# 
