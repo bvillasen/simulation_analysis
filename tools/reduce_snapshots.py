3139db312ee6f863c31f0401c79e6e3fb24abd6b
bruno <bvillasen@gmail.com>
2021-05-19 10:31:54 -0700

    new

--- tools/reduce_snapshots.py
+++ tools/reduce_snapshots.py
@@ -3,7 +3,6 @@ import numpy as np
 import time
 import h5py as h5
 from tools import *
-from load_data import load_snapshot_data_distributed
 
 use_mpi = False
 if use_mpi:
@@ -20,13 +19,14 @@ input_dir = data_dir + 'cosmo_sims/rescaled_P19/2048_50Mpc/snapshot_files/'
 output_dir = data_dir + 'cosmo_sims/rescaled_P19/2048_50Mpc/reduced_snapshots/'
 if rank == 0: create_directory( output_dir )
 
+n_snapshots = 1
+
+precision = np.float32
 
 
 file_counter = 0
-
-n_snapshots = 1
-
 time_start = time.time()
+
 
 n_snap = 0
 
@@ -38,8 +38,14 @@ if rank == 0: print(f'Reducing Snapshot: {n_snap }' )
 fields_hydro = [ 'density', 'temperature' ]
 fields_particles = [ 'density' ]
 
-if type == 'hydro': base_file_name = '.h5.'
-if type == 'particles': base_file_name = '_particles.h5.'
+if type == 'hydro': 
+  base_file_name = '.h5.'
+  fields_list = fields_hydro
+  
+if type == 'particles': 
+  base_file_name = '_particles.h5.'
+fields_list = fields_particles
+
 
 
 
@@ -57,20 +63,21 @@ n_total_local = n_snapshots * n_files_local
 for file_id in indices_local:
   file_name = files_snapshot[file_id]
   in_file  = h5.File( input_dir + file_name, 'r' )
-  # out_file = h5.File( output_dir + file_name, 'w' )
+  out_file = h5.File( output_dir + file_name, 'w' )
+    
+  for key in infile.attrs.keys():
+    out_file.attrs[key] = in_file.attrs[key]
+  
+  for field in fields_list:
+    data = in_file[field][...].astype( precision )
+    out_file.create_dataset( field, data=data )
   
   
   
   
   
-  
-  
-  
-  
-  
-  
-  # in_file.close()
-  # out_file.close() 
+  in_file.close()
+  out_file.close() 
   
   file_counter += 1
   if rank == 0: print_progress( file_counter, n_total_local, time_start )
