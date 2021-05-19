57a5e003782e3da1b1ef38b3670d67d76da8dc31
bruno <bvillasen@gmail.com>
2021-05-19 10:14:41 -0700

    new

--- tools/reduce_snapshots.py
+++ tools/reduce_snapshots.py
@@ -4,18 +4,28 @@ import h5py as h5
 from tools import *
 from load_data import load_snapshot_data_distributed
 
-
+use_mpi = False
+if use_mpi:
+  from mpi4py import MPI
+  comm = MPI.COMM_WORLD
+  rank = comm.Get_rank()
+  n_procs = comm.Get_size()
+else:
+  rank = 0
+  n_procs = 1
 
 data_dir = '/gpfs/alpine/csc434/scratch/bvilasen/'
 input_dir = data_dir + 'cosmo_sims/rescaled_P19/2048_50Mpc/snapshot_files/'
 output_dir = data_dir + 'cosmo_sims/rescaled_P19/2048_50Mpc/reduced_snapshots/'
-
+if rank == 0: create_directory( output_dir )
 
 n_snap = 0
 
 
 type = 'hydro'
 # type = 'particles'
+
+if rank == 0: print(f'Reducing Snapshot: {n_snap }' )
 
 fields_hydro = [ 'density', 'temperature' ]
 fields_particles = [ 'density' ]
@@ -26,6 +36,9 @@ if type == 'particles': base_file_name = '_particles.h5.'
 
 
 files_snapshot = [f for f in listdir(input_dir) if f.find(f'{n_snap}{base_file_name}') == 0 ]
+n_files = len( files_snapshot )
+
+if rank == 0: print(f'N files per snapshot: {n_files}')
 
 
 
