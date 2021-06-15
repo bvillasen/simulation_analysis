bceb5755a0d9ab6e71cb599a56634c142ed5f02b
bruno <bvillasen@gmail.com>
2021-06-14 17:54:00 -0700

    uodated

--- benchmarks/generate_scaling_files.py
+++ benchmarks/generate_scaling_files.py
@@ -9,8 +9,8 @@ from submit_job_scripts import Create_Submit_Job_Script_Summit
 
 GPUS_PER_NODE = 6
 
-# n_per_gpu = 128
-n_per_gpu = 256
+n_per_gpu = 128
+# n_per_gpu = 256
 
 
 n_mpi_list = [ 8, 64, 512, 1024  ]
