1cdb6785c4257aaae02e2d69927c93db743ff650
bruno <bvillasen@gmail.com>
2021-06-14 17:11:02 -0700

    updated

--- simulation_grid/submit_job_scripts.py
+++ simulation_grid/submit_job_scripts.py
@@ -22,8 +22,8 @@ def Create_Submit_Job_Script_Summit( job_params, save_file=True, file_name='subm
 #BSUB -e log_error.txt
 #BSUB -alloc_flags "smt4"
 
-module load gcc/10.2.0 cuda/11.2.0 fftw hdf5
-
+#module load gcc/10.2.0 cuda/11.2.0 fftw hdf5
+module load xl cuda fftw hdf5 python
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/ccs/home/bvilasen/code/fftw/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/ccs/home/bvilasen/code/grackle/lib
