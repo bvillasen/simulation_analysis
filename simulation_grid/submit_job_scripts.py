2cfb526251e7acaf94e57024de0dcfeac8b08408
bruno <bvillasen@gmail.com>
2021-06-16 11:37:42 -0700

    updaTED

--- simulation_grid/submit_job_scripts.py
+++ simulation_grid/submit_job_scripts.py
@@ -22,7 +22,6 @@ def Create_Submit_Job_Script_Summit( job_params, save_file=True, file_name='subm
 #BSUB -e log_error.txt
 #BSUB -alloc_flags "smt4"
 
-#module load gcc/10.2.0 cuda/11.2.0 fftw hdf5
 module load xl cuda fftw hdf5 python
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/ccs/home/bvilasen/code/fftw/lib
@@ -34,8 +33,9 @@ cd {root_dir}
 
 date
 export OMP_NUM_THREADS=7
-jsrun --smpiargs="-gpu" -n{n_mpi_tasks} -a1 -c7 -g1 --bind packed:7 $CHOLLA_HOME/cholla.paris.summit $WORK_DIR/param.txt > $WORK_DIR/run_output.log |sort
+jsrun --smpiargs="-gpu" -n{n_mpi_tasks} -a1 -c7 -g1 --bind packed:7 $CHOLLA_HOME/cholla.FOM.summit $WORK_DIR/param.txt > $WORK_DIR/run_output.log |sort
 """
+#module load gcc/10.2.0 cuda/11.2.0 fftw hdf5
 # jsrun -n {n_mpi_tasks} -a 1 -c 7 -g 1 -l CPU-CPU -d packed -b packed:7 $CHOLLA_HOME/cholla $WORK_DIR/param.txt > $WORK_DIR/output.log |sort
 # module load xl cuda fftw hdf5
 # module load gcc/6.4.0
--- tools/merge_snapshot_slices.py
+++ tools/merge_snapshot_slices.py
@@ -14,19 +14,19 @@ sys.path.extend(subDirectories)
 from load_data import load_snapshot_data_distributed
 from tools import *
 
-params_type = 'H' 
-# params_type = 'He'
+np.random.seed(1234)
+
+# params_type = 'H' 
+params_type = 'He'
 
 data_dir = '/raid/bruno/data/' 
 # data_dir = '/data/groups/comp-astro/bruno/'
-root_dir  = data_dir + f'cosmo_sims/sim_grid/1024_P19m_np4_nsim400/selected_snapshot_files_params_{params_type}/slices/'
+root_dir  = data_dir + f'cosmo_sims/sim_grid/1024_P19m_np4_nsim400/slices_params_{params_type}/'
 output_dir = root_dir
 create_directory( output_dir )
 
-scale_He_vals = []
-deltaZ_He_vals = []
 
-n_snap = 2
+n_snap = 8
 slice_depth = 64
 slice_start = 192
 
@@ -34,141 +34,179 @@ slice_start = 192
 if params_type == 'He': field = 'temperature'
 if params_type == 'H':  field = 'HI_density'
 
-extend = True
+n_sims = 400
+sim_ids = list( range( n_sims ) )
+np.random.shuffle( sim_ids )
 
-n_sims = 20
-
-param_vals_x, param_vals_y = [], []
-
-data_sims = {}
-for sim_id in range( n_sims ):
-  input_dir = root_dir + f'sim_{sim_id}/'
-  file_name = input_dir  + 'data_sim.pkl'
-  sim_data = Load_Pickle_Directory( file_name )
-  data_sims[sim_id] = sim_data
-  if params_type == 'He':
-    scale_He  = sim_data['parameters']['scale_He']
-    deltaZ_He = sim_data['parameters']['deltaZ_He']
-    param_vals_x.append( scale_He )
-    param_vals_y.append( deltaZ_He )  
-  if params_type == 'H':
-    scale_H  = sim_data['parameters']['scale_H']
-    deltaZ_H = sim_data['parameters']['deltaZ_H']
-    param_vals_x.append( scale_H )
-    param_vals_y.append( deltaZ_H )
-  in_file_name = input_dir + f'slice_{n_snap}_start{slice_start}_depth{slice_depth}.h5'
-  in_file = h5.File( in_file_name,  'r' )
-  current_z = in_file.attrs['current_z']
-  slice = in_file[field][...] / scale_H
-  if extend:
-    nz, ny, nx = slice.shape
-    slice_extended = np.zeros( (nz, 2*ny, 2*nx) )
-    for i in range( 2):
-      for j in range(2):
-        slice_extended[:, i*ny:(i+1)*ny,  j*nx:(j+1)*nx] = slice
-    slice = slice_extended
-  data_sims[sim_id]['slice'] = slice
-  in_file.close()
-
-param_vals_x  = np.array( list( set( param_vals_x ) ) )
-param_vals_y = np.array( list( set( param_vals_y) ) )
-param_vals_x.sort()
-param_vals_y.sort()
+n_sims_y, n_sims_x = 10, 10
 
 
-nrows, ncols = len(param_vals_y), len(param_vals_x)
-indices_2D = np.ones( (nrows, ncols), dtype=np.int ) * -1  
-params_x = np.zeros( (nrows, ncols) )
-params_y = np.zeros( (nrows, ncols) )
+have_slice_extended = False
 
+for i in range( n_sims_y ):
+  for j in range( n_sims_x ):
+    indx = i * n_sims_x + j
+    sim_id = sim_ids[indx]
+    print( f'Ids: {i}  {j}   sim_id{sim_id}') 
+    in_file_name  = root_dir  + f'sim_{sim_id}/slice_{n_snap}_start{slice_start}_depth{slice_depth}.h5'
+    print( f'Loading File: {in_file_name }' )
+    in_file = h5.File( in_file_name, 'r' )
+    
+    slice = in_file[field][...]
+    slice_size = slice.shape
+    in_file.close()
+    
+    if not have_slice_extended:
+      size_extended = ( slice_size[0] , n_sims_y * slice_size[1], n_sims_x * slice_size[2] )
+      print( f' Extended Size: {size_extended}'  )
+      slice_extended = np.zeros( size_extended )
+      have_slice_extended = True
+      
+    d, h, w = slice_size
+    slice_extended[:, i*h:(i+1)*h,  j*w:(j+1)*w] = slice 
+    
+    break
+  break
+    
+     
+    
 
-for sim_id in data_sims:
-  sim_data = data_sims[sim_id]
-  scale_He  = sim_data['parameters']['scale_He']
-  deltaZ_He = sim_data['parameters']['deltaZ_He']
-  scale_H   = sim_data['parameters']['scale_H']
-  deltaZ_H  = sim_data['parameters']['deltaZ_H']
-  if params_type == 'He':
-    param_x = scale_He
-    param_y = deltaZ_He
-  if params_type == 'H':
-    param_x = scale_H
-    param_y = deltaZ_H
-  indx_x = np.where( param_vals_x == param_x )[0]
-  indx_y = np.where( param_vals_y == param_y )[0]
-  if len(indx_x) != 1: print(f'ERROR: indx_x: {indx_x}' )
-  if len(indx_y) != 1: print(f'ERROR: indx_x: {indx_y}' ) 
-  indices_2D[indx_y,indx_x] = sim_id   
-  params_x[indx_y,indx_x] = param_x
-  params_y[indx_y,indx_x] = param_y
-
-
-slice = data_sims[0]['slice']
-nz, ny, nx = slice.shape
-n_vals_x = len(param_vals_x)
-n_vals_y = len(param_vals_y)
-
-n_vals_x, n_vals_y = 2, 2
-
-n_split_x, n_split_y = n_vals_x-1, n_vals_y-1 
-delta_x, delta_y = nx//n_split_x, ny//n_split_y
-print( f'Slice Shape: {slice.shape} ' )
-print( f'N split   x:{n_split_x}   y:{n_split_y}' )
-
-slice_new = image = np.zeros_like( slice )
-
-
-time_start = time.time()
-for i in range( ny ):
-  for j in range( nx ):
-    id = i*nx + j
-    pos_x, pos_y = j, i
-    indx_x, indx_y = pos_x // delta_x, pos_y//delta_y
-
-    if indx_x == n_vals_x - 1: indx_x -= 1
-    if indx_y == n_vals_y - 1: indx_y -= 1
-
-
-    id_ld = indices_2D[ indx_y,             indx_x   ]
-    id_lu = indices_2D[ indx_y+n_vals_y -1, indx_x   ]
-    id_rd = indices_2D[ indx_y,             indx_x+n_vals_x -1  ]
-    id_ru = indices_2D[ indx_y+n_vals_y -1, indx_x+n_vals_x -1 ] 
-
-    val_ld = data_sims[id_ld]['slice'][:, i, j]
-    val_lu = data_sims[id_lu]['slice'][:, i, j]
-    val_rd = data_sims[id_rd]['slice'][:, i, j]
-    val_ru = data_sims[id_ru]['slice'][:, i, j]
-
-    x, y = pos_x % delta_x, pos_y % delta_y
-    alpha_x, alpha_y = x / delta_x, y / delta_y
-
-
-    diff_x_d = val_rd - val_ld 
-    diff_x_u = val_ru - val_lu 
-    interp_x_d = val_ld + alpha_x*diff_x_d 
-    interp_x_u = val_lu + alpha_x*diff_x_u
-    interp = interp_x_d + alpha_y * ( interp_x_u - interp_x_d )
-    slice_new[:, i, nx-j-1] = interp
-
-    if id%16 == 0: print_progress( id+1, nx*ny, time_start )
-
-
-out_file_name = output_dir + f'slice_interp_{n_snap}_start{slice_start}_depth{slice_depth}'
-
-if extend: out_file_name += '_extended'
-out_file_name += '.h5'
-
-outfile = h5.File( out_file_name, 'w' )
-outfile.attrs['current_z'] = current_z
-outfile.create_dataset( field, data=slice_new )
-outfile.close()
-print( f'Saved File: {out_file_name}' )
+    
+out_file_name = 
 
 
 
-
-
-
-
-
-
+# param_vals_x, param_vals_y = [], []
+# 
+# data_sims = {}
+# for sim_id in range( n_sims ):
+#   input_dir = root_dir + f'sim_{sim_id}/'
+#   file_name = input_dir  + 'data_sim.pkl'
+#   sim_data = Load_Pickle_Directory( file_name )
+#   data_sims[sim_id] = sim_data
+#   if params_type == 'He':
+#     scale_He  = sim_data['parameters']['scale_He']
+#     deltaZ_He = sim_data['parameters']['deltaZ_He']
+#     param_vals_x.append( scale_He )
+#     param_vals_y.append( deltaZ_He )  
+#   if params_type == 'H':
+#     scale_H  = sim_data['parameters']['scale_H']
+#     deltaZ_H = sim_data['parameters']['deltaZ_H']
+#     param_vals_x.append( scale_H )
+#     param_vals_y.append( deltaZ_H )
+#   in_file_name = input_dir + f'slice_{n_snap}_start{slice_start}_depth{slice_depth}.h5'
+#   in_file = h5.File( in_file_name,  'r' )
+#   current_z = in_file.attrs['current_z']
+#   slice = in_file[field][...] / scale_H
+#   if extend:
+#     nz, ny, nx = slice.shape
+#     slice_extended = np.zeros( (nz, 2*ny, 2*nx) )
+#     for i in range( 2):
+#       for j in range(2):
+#         slice_extended[:, i*ny:(i+1)*ny,  j*nx:(j+1)*nx] = slice
+#     slice = slice_extended
+#   data_sims[sim_id]['slice'] = slice
+#   in_file.close()
+# 
+# param_vals_x  = np.array( list( set( param_vals_x ) ) )
+# param_vals_y = np.array( list( set( param_vals_y) ) )
+# param_vals_x.sort()
+# param_vals_y.sort()
+# 
+# 
+# nrows, ncols = len(param_vals_y), len(param_vals_x)
+# indices_2D = np.ones( (nrows, ncols), dtype=np.int ) * -1  
+# params_x = np.zeros( (nrows, ncols) )
+# params_y = np.zeros( (nrows, ncols) )
+# 
+# 
+# for sim_id in data_sims:
+#   sim_data = data_sims[sim_id]
+#   scale_He  = sim_data['parameters']['scale_He']
+#   deltaZ_He = sim_data['parameters']['deltaZ_He']
+#   scale_H   = sim_data['parameters']['scale_H']
+#   deltaZ_H  = sim_data['parameters']['deltaZ_H']
+#   if params_type == 'He':
+#     param_x = scale_He
+#     param_y = deltaZ_He
+#   if params_type == 'H':
+#     param_x = scale_H
+#     param_y = deltaZ_H
+#   indx_x = np.where( param_vals_x == param_x )[0]
+#   indx_y = np.where( param_vals_y == param_y )[0]
+#   if len(indx_x) != 1: print(f'ERROR: indx_x: {indx_x}' )
+#   if len(indx_y) != 1: print(f'ERROR: indx_x: {indx_y}' ) 
+#   indices_2D[indx_y,indx_x] = sim_id   
+#   params_x[indx_y,indx_x] = param_x
+#   params_y[indx_y,indx_x] = param_y
+# 
+# 
+# slice = data_sims[0]['slice']
+# nz, ny, nx = slice.shape
+# n_vals_x = len(param_vals_x)
+# n_vals_y = len(param_vals_y)
+# 
+# n_vals_x, n_vals_y = 2, 2
+# 
+# n_split_x, n_split_y = n_vals_x-1, n_vals_y-1 
+# delta_x, delta_y = nx//n_split_x, ny//n_split_y
+# print( f'Slice Shape: {slice.shape} ' )
+# print( f'N split   x:{n_split_x}   y:{n_split_y}' )
+# 
+# slice_new = image = np.zeros_like( slice )
+# 
+# 
+# time_start = time.time()
+# for i in range( ny ):
+#   for j in range( nx ):
+#     id = i*nx + j
+#     pos_x, pos_y = j, i
+#     indx_x, indx_y = pos_x // delta_x, pos_y//delta_y
+# 
+#     if indx_x == n_vals_x - 1: indx_x -= 1
+#     if indx_y == n_vals_y - 1: indx_y -= 1
+# 
+# 
+#     id_ld = indices_2D[ indx_y,             indx_x   ]
+#     id_lu = indices_2D[ indx_y+n_vals_y -1, indx_x   ]
+#     id_rd = indices_2D[ indx_y,             indx_x+n_vals_x -1  ]
+#     id_ru = indices_2D[ indx_y+n_vals_y -1, indx_x+n_vals_x -1 ] 
+# 
+#     val_ld = data_sims[id_ld]['slice'][:, i, j]
+#     val_lu = data_sims[id_lu]['slice'][:, i, j]
+#     val_rd = data_sims[id_rd]['slice'][:, i, j]
+#     val_ru = data_sims[id_ru]['slice'][:, i, j]
+# 
+#     x, y = pos_x % delta_x, pos_y % delta_y
+#     alpha_x, alpha_y = x / delta_x, y / delta_y
+# 
+# 
+#     diff_x_d = val_rd - val_ld 
+#     diff_x_u = val_ru - val_lu 
+#     interp_x_d = val_ld + alpha_x*diff_x_d 
+#     interp_x_u = val_lu + alpha_x*diff_x_u
+#     interp = interp_x_d + alpha_y * ( interp_x_u - interp_x_d )
+#     slice_new[:, i, nx-j-1] = interp
+# 
+#     if id%16 == 0: print_progress( id+1, nx*ny, time_start )
+# 
+# 
+# out_file_name = output_dir + f'slice_interp_{n_snap}_start{slice_start}_depth{slice_depth}'
+# 
+# if extend: out_file_name += '_extended'
+# out_file_name += '.h5'
+# 
+# outfile = h5.File( out_file_name, 'w' )
+# outfile.attrs['current_z'] = current_z
+# outfile.create_dataset( field, data=slice_new )
+# outfile.close()
+# print( f'Saved File: {out_file_name}' )
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
