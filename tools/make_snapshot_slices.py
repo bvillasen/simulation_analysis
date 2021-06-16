e8b04739ac5be1809b52fc34238ee423b3f64fd0
bruno <bvillasen@gmail.com>
2021-06-16 10:19:48 -0700

    updated

--- tools/make_snapshot_slices.py
+++ tools/make_snapshot_slices.py
@@ -49,67 +49,56 @@ if rank == 0: print( f'Sims Local: {sim_ids_local}' )
 
 for sim_id in sim_ids_local:
   sim_file = sim_files[sim_id]
-  if rank == 0: print( sim_file ) 
+  
 
-# 
-# input_dir  = data_dir + f'cosmo_sims/sim_grid/1024_P19m_np4_nsim400/sim_{sim_id}/'
-# output_dir = data_dir + f'cosmo_sims/sim_grid/1024_P19m_np4_nsim400/slices_params_{params_type}/sim_{sim_id}/'
-# # input_dir  = data_dir + f'cosmo_sims/sim_grid/1024_P19m_np4_nsim400/selected_snapshot_files_params_{params_type}/sim_{sim_id}/'
-# # output_dir = data_dir + f'cosmo_sims/sim_grid/1024_P19m_np4_nsim400/selected_snapshot_files_params_{params_type}/slices/sim_{sim_id}/'
-# create_directory( output_dir )
-# 
-# n_points = 1024
-# Lbox = 50000.0 #kpc/h
-# box_size = [ Lbox, Lbox, Lbox ]
-# grid_size = [ n_points, n_points, n_points ]
-# precision = np.float32
-# 
-# 
-# n_snap = 8
-# if params_type == 'He': fields =  ['temperature']  
-# if params_type == 'H': fields =  ['density', 'HI_density']  
-# data_gas = load_snapshot_data_distributed( 'hydro', fields, n_snap, input_dir, box_size, grid_size,  precision, show_progess=show_progess )
-# current_z = data_gas['Current_z']
-# 
-# slice_depth = 64
-# n_slices = n_points // slice_depth
-# print( f'N slices: {n_slices}' )
-# 
-# 
-# # for slice_id in range( n_slices ): 
-# slice_id = 3
-# slice_start = slice_id * slice_depth
-# 
-# start = max( 0, slice_start )
-# end   = min( n_points, slice_start+slice_depth )
-# print( f' Slice:  start:{start}   end:{end}' )
-# 
-# out_file_name = output_dir + f'slice_{n_snap}_start{slice_start}_depth{slice_depth}.h5'
-# outfile = h5.File( out_file_name, 'w' )
-# outfile.attrs['current_z'] = current_z
-# 
-# for field in fields:
-#   data = data_gas[field]
-#   data_slice = data[slice_start:end, :, :] 
-#   outfile.create_dataset( field, data=data_slice )
-# 
-# outfile.close()
-# print( f'Saved File: {out_file_name}' )
-# 
-# 
-# # src = input_dir  + 'data_sim.pkl'
-# # dst = output_dir + 'data_sim.pkl'
-# # copyfile(src, dst)
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
+  input_dir  = data_dir + f'cosmo_sims/sim_grid/1024_P19m_np4_nsim400/{sim_file}/'
+  output_dir = data_dir + f'cosmo_sims/sim_grid/1024_P19m_np4_nsim400/slices_params_{params_type}/sim_{sim_id}/'
+  # input_dir  = data_dir + f'cosmo_sims/sim_grid/1024_P19m_np4_nsim400/selected_snapshot_files_params_{params_type}/sim_{sim_id}/'
+  # output_dir = data_dir + f'cosmo_sims/sim_grid/1024_P19m_np4_nsim400/selected_snapshot_files_params_{params_type}/slices/sim_{sim_id}/'
+  create_directory( output_dir )
+
+  n_points = 1024
+  Lbox = 50000.0 #kpc/h
+  box_size = [ Lbox, Lbox, Lbox ]
+  grid_size = [ n_points, n_points, n_points ]
+  precision = np.float32
+
+
+  n_snap = 8
+  if params_type == 'He': fields =  ['temperature']  
+  if params_type == 'H': fields =  ['density', 'HI_density']  
+  data_gas = load_snapshot_data_distributed( 'hydro', fields, n_snap, input_dir, box_size, grid_size,  precision, show_progess=show_progess )
+  current_z = data_gas['Current_z']
+
+  slice_depth = 64
+  n_slices = n_points // slice_depth
+  # if rank == 0: print( f'N slices: {n_slices}' )
+
+
+  # for slice_id in range( n_slices ): 
+  slice_id = 3
+  slice_start = slice_id * slice_depth
+
+  start = max( 0, slice_start )
+  end   = min( n_points, slice_start+slice_depth )
+  # print( f' Slice:  start:{start}   end:{end}' )
+
+  out_file_name = output_dir + f'slice_{n_snap}_start{slice_start}_depth{slice_depth}.h5'
+  outfile = h5.File( out_file_name, 'w' )
+  outfile.attrs['current_z'] = current_z
+
+  for field in fields:
+    data = data_gas[field]
+    data_slice = data[slice_start:end, :, :] 
+    outfile.create_dataset( field, data=data_slice )
+
+  outfile.close()
+  print( f'Saved File: {out_file_name}' )
+  
+  break
+
+
+  # src = input_dir  + 'data_sim.pkl'
+  # dst = output_dir + 'data_sim.pkl'
+  # copyfile(src, dst)
+
