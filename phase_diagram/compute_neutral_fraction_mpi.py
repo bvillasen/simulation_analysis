dfb959ea055f84b99a526c573aa9e59274f516d6
bruno villasenor <bvillasen@gmail.com>
2021-06-19 12:38:21 -0700

    updated

--- phase_diagram/compute_neutral_fraction_mpi.py
+++ phase_diagram/compute_neutral_fraction_mpi.py
@@ -36,69 +36,69 @@ n_snap = 169
 H0 = 67.66
 Omega_b =  0.0497 
 h = H0 / 100
+X = 0.75984603480
 
 rho_crit =  3*(H0*1e-3)**2/(8*np.pi*G_COSMO)/ h**2
 rho_gas_mean = rho_crit * Omega_b 
 dens_max = 3 * rho_gas_mean
 
 
-chem_type = 'HI'
-chem_type = 'HeI'
-chem_dens_name = f'{chem_type}_density'
-
-X = 0.75984603480
-if chem_type == 'HI': chem_fraction = X 
-else: chem_fraction = 1 - X
-
-
-
 files_per_snapshot = 512
-
-
-
 indices_local = split_indices( range(files_per_snapshot), rank, n_procs )
 
-n_total_local = 0
-n_samples_local = 0
-fraction_sum = 0  
 
-for indx in indices_local:
+chem_types = [ 'HI', 'HeI', 'HeII'  ]
 
-  n_file = indx
-  in_file_name = input_dir + f'{n_snap}.h5.{n_file}'
-  in_file = h5.File( in_file_name, 'r' )
-  # print( in_file.keys( ) )
-  current_z = in_file.attrs['Current_z'][0]
+for chem_type in chem_types:
+  chem_dens_name = f'{chem_type}_density'
 
-  density = in_file['density'][...]
-  n_cells_local = np.prod( density.shape )
-  chem_density = in_file[chem_dens_name][...]
-  indices = density <= dens_max
-  density = density[indices] * chem_fraction
-  chem_density = chem_density[indices]
-  dens_fraction = chem_density / density 
-  fraction_sum += dens_fraction.sum()
-  n_samples_local +=  indices.sum()
-  n_total_local += n_cells_local
+  if chem_type == 'HI': chem_fraction = X 
+  else: chem_fraction = 1 - X
 
 
 
-#Send the phase diagram to root process
-fraction_all = comm.gather( fraction_sum, root=0 )
-n_local_all  = comm.gather( n_samples_local, root=0 )
-n_total_all  = comm.gather( n_total_local, root=0 )
+
+  n_total_local = 0
+  n_samples_local = 0
+  fraction_sum = 0  
+
+  for indx in indices_local:
+
+    n_file = indx
+    in_file_name = input_dir + f'{n_snap}.h5.{n_file}'
+    in_file = h5.File( in_file_name, 'r' )
+    # print( in_file.keys( ) )
+    current_z = in_file.attrs['Current_z'][0]
+
+    density = in_file['density'][...]
+    n_cells_local = np.prod( density.shape )
+    chem_density = in_file[chem_dens_name][...]
+    indices = density <= dens_max
+    density = density[indices] * chem_fraction
+    chem_density = chem_density[indices]
+    dens_fraction = chem_density / density 
+    fraction_sum += dens_fraction.sum()
+    n_samples_local +=  indices.sum()
+    n_total_local += n_cells_local
 
 
-if rank == 0:
-  fraction_all = np.array( fraction_all )
-  n_local_all = np.array( n_local_all )
-  n_total_all = np.array( n_total_all )
-  fraction_sum_global = fraction_all.sum()
-  n_local_global = n_local_all.sum()
-  chem_fraction_global = fraction_sum_global / n_local_global
-  n_total_global = n_total_all.sum()
-  print( f'{chem_type} N_total:{n_total_global}    N_samples:{n_local_global}   {n_local_global/n_total_global}  ' )  
-  print( f'{chem_type} Fraction: {chem_fraction_global} ' )
+
+  #Send the phase diagram to root process
+  fraction_all = comm.gather( fraction_sum, root=0 )
+  n_local_all  = comm.gather( n_samples_local, root=0 )
+  n_total_all  = comm.gather( n_total_local, root=0 )
+
+
+  if rank == 0:
+    fraction_all = np.array( fraction_all )
+    n_local_all = np.array( n_local_all )
+    n_total_all = np.array( n_total_all )
+    fraction_sum_global = fraction_all.sum()
+    n_local_global = n_local_all.sum()
+    chem_fraction_global = fraction_sum_global / n_local_global
+    n_total_global = n_total_all.sum()
+    print( f'{chem_type} N_total:{n_total_global}    N_samples:{n_local_global}   {n_local_global/n_total_global}  ' )  
+    print( f'{chem_type} Fraction: {chem_fraction_global} ' )
 
 
 
